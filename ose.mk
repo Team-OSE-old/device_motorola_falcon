d644a49b4e15b799028cfd877f8e6c2ec0c7ee8c
Tim Pohlad-Thomas <timpohladthomas@gmail.com>
2014-07-19 08:43:44 -0400

    Update and rename slim.mk to ose.mk

--- ose.mk
+++ ose.mk
@@ -1,5 +1,5 @@
 # Slim
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/ose/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -7,6 +7,6 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := slim_falcon
+PRODUCT_NAME := ose_falcon
 
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
